fb728fcde11bb4f8df334ffa56bb35230531f7af
tmerlier <theophile.merliere@gmail.com>
2020-03-31 17:37:19 +0200

    Change default viewport zoom

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ const reportToGeoJSON = (report, date) => {
 const defaultViewport = {
   latitude: 46.9,
   longitude: 1.7,
-  zoom: 5
+  zoom: 4.5
 }
 
 const MainPage = ({data, dates, isGouv}) => {
@@ -159,7 +159,7 @@ const MainPage = ({data, dates, isGouv}) => {
     setViewport({
       latitude,
       longitude,
-      zoom: isMobileDevice ? 4.3 : 5
+      zoom: isMobileDevice ? 4.3 : defaultViewport.zoom
     })
   }, [isMobileDevice]) // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -278,6 +278,7 @@ const MainPage = ({data, dates, isGouv}) => {
           previousFranceReport,
           regionsReport,
           departementsReport,
+          getDROMReport,
           prev: dateIdx > 0 ? previousReport : null,
           next: dateIdx < dates.length - 1 ? nextReport : null,
           setViewport,
